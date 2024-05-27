14730d25ee6e5400539e5fefc8ba443e18ad98c5
Matetorture <przemek.pytlarz18@gmail.com>
2024-05-27 15:34:08 +0200

    Added cors to api

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const Message = require('./models/message.model');
 
@@ -8,6 +9,9 @@ const userRoute = require('./routes/user.route');
 
 
 const app = express();
+app.use(cors());
+
+
 app.use(express.json());
 
 //routes
