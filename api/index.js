1b30230a6c009827d651afb25e675c57bb1830e7
Matetorture <przemek.pytlarz18@gmail.com>
2024-06-02 17:17:38 +0200

    Removed unnessesary thing

--- api/index.js
+++ api/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const Message = require('./models/message.model');
-
 const messageRoute = require('./routes/message.route');
 const userRoute = require('./routes/user.route');
 
