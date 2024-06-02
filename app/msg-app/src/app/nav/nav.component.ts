0dc39da7106b3748985c45291cad59eb907409c2
Matetorture <przemek.pytlarz18@gmail.com>
2024-06-02 13:57:37 +0200

    Added theme mode

--- app/msg-app/src/app/nav/nav.component.ts
+++ app/msg-app/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 import { CookieService } from '../service/cookie/cookie.service';
@@ -6,7 +7,7 @@ import { CookieService } from '../service/cookie/cookie.service';
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
@@ -18,5 +19,26 @@ export class NavComponent {
   logout(){
     this.cookieService.set("userId", "");
     this.router.navigate(['login']);
+  }
+
+  ngOnInit(): void {
+    if(this.cookieService.get("theme") == ""){
+      this.cookieService.set("theme", "dark");
+    }
+    if(this.cookieService.get("theme") == "dark"){
+      this.changeThemeMode();
+    }
+  }
+
+  isDarkTheme: boolean = false;
+
+  changeThemeMode() {
+    document.body.classList.toggle('dark');
+    this.isDarkTheme = !this.isDarkTheme;
+    if(this.isDarkTheme){
+      this.cookieService.set("theme", "dark");
+    }else{
+      this.cookieService.set("theme", "light");
+    }
   }
 }
