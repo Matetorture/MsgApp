a52308eeea5191a098fe32a897b5432f98c0a268
Matetorture <przemek.pytlarz18@gmail.com>
2024-06-02 17:34:19 +0200

    Added working online status api

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+const User = require("./models/user.model");
+
 const messageRoute = require('./routes/message.route');
 const userRoute = require('./routes/user.route');
 
@@ -21,6 +23,15 @@ const {connectionString} = require("./config");
 mongoose.connect(connectionString)
 .then(() => {
     console.log("Connected to database!");
+
+    setInterval(async () => {
+        const users = await User.find({});
+
+        users.forEach(async (e) => {
+            const user = await User.findByIdAndUpdate(e._id, {status: ""});
+        });
+    }, 60 * 1000);
+
     app.listen(3000, () => {
         console.log('run 3000');
     });
